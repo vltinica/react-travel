80473bfb91467966e4c18ba94f27840fe066512d
Vladimir Tinica <vltinica@gmail.com>
2023-03-19 12:34:19 -0400

    commponent Destination

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./index.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Destinations from "./components/Destinations";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Destinations />
     </div>
   );
 };
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,12 +12,12 @@ const Hero = () => {
         muted
         src={beachVid}
       />
-      <div className="w-full h-full top-0 left-0 bg-gray-900/30"></div>
+      <div className="w-full h-full top-0 left-0 bg-gray-700/10"></div>
       <div className="absolute top-0 w-full h-full flex flex-col justify-center text-center text-white p-6">
         <h1>First Class Travel</h1>
-        <h2>Top Locations Worldwide</h2>
+        <h2 className="py-4">Top Locations Worldwide</h2>
         <form
-          className="flex justify-between items-center max-w-[700] mx-auto w-full border p-1 rounded-md
+          className="flex justify-between items-center max-w-[550px] mx-auto w-full border p-1 rounded-md
          text-black bg-gray-100/70"
         >
           <div>
@@ -29,7 +29,11 @@ const Hero = () => {
           </div>
           <div>
             <button>
-              <AiOutlineSearch size={20} className="icon" />
+              <AiOutlineSearch
+                size={20}
+                className="icon"
+                style={{ color: "#ffffff" }}
+              />
             </button>
           </div>
         </form>
