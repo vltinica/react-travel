08b688b4466fcc7ce3630703fb91b2e4d5491461
Vladimir Tinica <vltinica@gmail.com>
2023-03-19 12:51:54 -0400

    start of Search commponent

--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -8,10 +8,10 @@ import KeyWest from "../assets/keywest.jpg";
 
 const Destinations = () => {
   return (
-    <div className="max-w-[1240px] py-16 px-4 text-center">
+    <div className="max-w-[1240px]  mx-auto py-16 px-4 text-center">
       <h1>All-Inclusive Resorts</h1>
       <p className="py-4">On the Caribbean's Best Beaches</p>
-      <div className="grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4">
+      <div className="grid grid-rows-none md:grid-cols-5 py-4 gap-2 md:gap-4 ">
         <img
           className="w-full h-full object-cover  col-span-2 md:col-span-3 row-span-2"
           src={BoraBora}
